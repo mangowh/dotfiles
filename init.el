4c9919a98decaad5f29239270cf0155bdbba8e3b
Stefano <17550149+mangowh@users.noreply.github.com>
2025-12-21 18:09:13 +0100

    Remove unnecessary comments and clean up init.el

--- init.el
+++ init.el
@@ -27,15 +27,4 @@
 (set-default-coding-systems 'utf-8)
 
 (global-display-line-numbers-mode 1)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(ace-window)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
