673fa005a01069c17d24ccb9075c39a456061018
Stefano <17550149+mangowh@users.noreply.github.com>
2025-12-21 19:47:34 +0100

    Refactor init.el for better organization and clarity

--- init.el
+++ init.el
@@ -1,20 +1,38 @@
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file 'noerror)
+
+;;; packages
 (package-initialize)
 
+;; ensure use-package is available
+(require 'use-package)
+
+;; custom packages
 (use-package ace-window
+  :ensure t
   :bind (("M-o" . ace-window)))
 
+;;; ui
+
+;; moving between buffers
 (windmove-default-keybindings 'meta)
 
-(setq visible-bell 1)
+;; disable beeping or blinking
+(setq visible-bell nil)
+(setq ring-bell-function #'ignore)
 
+;; custom theme
 (load-theme 'manoj-dark t)
 
+;; custom font
 (set-face-attribute 'default nil
                     :family "Atkinson Hyperlegible Mono"
                     :height 140)
-
 (setq-default line-spacing 0.2)
 
+;;; coding
+
+;; font
 (setq-default tab-width 2)
 (setq-default indent-tabs-mode nil)
 
@@ -23,8 +41,9 @@
 
 (setq-default prettify-symbols-mode nil)
 
-(set-language-environment "UTF-8")
-(set-default-coding-systems 'utf-8)
-
 (global-display-line-numbers-mode 1)
 
+;;; misc
+
+;; allow for shorter responses: "y" for yes and "n" for no.
+(setq read-answer-short t)
