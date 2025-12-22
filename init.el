ff700c84314570ad73bbcc428baa20ff3947e047
Stefano <17550149+mangowh@users.noreply.github.com>
2025-12-22 16:19:54 +0100

    Add custom.el for user settings and autosave
    Added custom.el file for user configurations and autosave settings.

--- init.el
+++ init.el
@@ -1,5 +1,12 @@
+;; custom.el file
+
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (load custom-file 'noerror)
+
+;; autosave
+
+(setq backup-directory-alist
+      `(("." . ,(concat user-emacs-directory "backups"))))
 
 ;; help
 
@@ -22,7 +29,6 @@
                     :family "Atkinson Hyperlegible Mono"
                     :height 140)
 (setq-default line-spacing 0.2)
-
 ;; package
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
@@ -30,10 +36,12 @@
              '("melpa-stable" . "https://stable.melpa.org/packages/"))
 (package-initialize)
 
-;; ensure use-package is available
+;; use-package
 (require 'use-package)
 
-;; custom packages
+(use-package company
+  :ensure t)
+
 (use-package ace-window
   :ensure t
   :bind (("M-o" . ace-window)))
@@ -75,3 +83,5 @@
 
 (column-number-mode 1)
 (global-hl-line-mode 1)
+(global-auto-revert-mode 1)
+(desktop-save-mode 1)
