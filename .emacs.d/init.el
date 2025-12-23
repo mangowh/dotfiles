3cb21b9c5f7471b294eaaa3f65226ff37dcd5f24
Stefano Tambellini <17550149+mangowh@users.noreply.github.com>
2025-12-23 23:46:27 +0100

    sync to home folder

---
+++ .emacs.d/init.el
@@ -0,0 +1,54 @@
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file 'noerror)
+
+;; ensure use-package is available
+(require 'use-package)
+
+;; custom packages
+(use-package ace-window
+  :ensure t
+  :bind (("M-o" . ace-window)))
+
+;;; ui
+
+;; moving between buffers
+(windmove-default-keybindings 'meta)
+
+;; disable beeping or blinking
+(setq visible-bell nil)
+(setq ring-bell-function #'ignore)
+
+;; custom theme
+(load-theme 'manoj-dark t)
+
+;; custom font
+(set-face-attribute 'default nil
+                    :family "Atkinson Hyperlegible Mono"
+                    :height 140)
+(setq-default line-spacing 0.2)
+
+;;; coding
+
+;; lua
+(setq treesit-language-source-alist
+      '((lua "https://github.com/MunifTanjim/tree-sitter-lua")))
+
+(add-to-list 'auto-mode-alist '("\\.lua\\'" . lua-ts-mode))
+
+;; font
+(setq-default tab-width 2)
+(setq-default indent-tabs-mode nil)
+
+(setq-default fill-column 80)
+(setq-default truncate-lines t)
+
+(setq-default prettify-symbols-mode nil)
+
+(global-display-line-numbers-mode 1)
+
+;; langs
+
+;;; misc
+
+;; allow for shorter responses: "y" for yes and "n" for no.
+(setq read-answer-short t)
